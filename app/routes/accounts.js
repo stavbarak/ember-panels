89031a0af7158b33638b25b9da0116ea7902f47f
stav <stavbarak@gmail.com>
2017-12-25 20:52:48 +0200

    presenting json data

---
+++ app/routes/accounts.js
@@ -0,0 +1,22 @@
+/* eslint-disable */
+import Route from '@ember/routing/route';
+
+export default Route.extend({
+  model() {
+    var headers = new Headers({
+      'Content-Type': 'application/json'
+    });
+    var request = new Request("/accounts.json", {
+      headers
+    })
+    return new Promise(function(resolve, reject) {
+      fetch(request)
+        .then(function(response) {
+          response.json().then((obj) => {
+            console.dir(obj)
+            resolve(obj)
+          })
+        });
+    })
+  }
+});

9e7689b7afb04468916cbf9cfd62401f143fe9de
stav <stavbarak@gmail.com>
2017-12-26 21:18:33 +0200

    override ember's pluralize functionality

---
+++ app/models/custom-inflector-rules.js
@@ -0,0 +1,7 @@
+import Inflector from 'ember-inflector';
+
+const inflector = Inflector.inflector;
+
+inflector.irregular('account', 'account');
+
+export default {};
--- app/routes/account.js
+++ app/routes/account.js
@@ -2,10 +2,7 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
   model(params) {
-    //trying to figure out this code and why does it try to get /accounts/3 instead of /account/3
-    console.log(params);
-    console.log(this.store.findRecord);
-    console.log(this.store.findRecord('account', params.account_id));
-    //return this.store.findRecord('account', params.account_id);
+    var p = params;
+    return this.store.findRecord('account', p.account_id);
   }
 });

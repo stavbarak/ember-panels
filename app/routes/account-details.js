d2b33ca5d629617fe8fc215490546fad90b45a5c
stav <stavbarak@gmail.com>
2017-12-27 17:05:19 +0200

    account form, still with a response error

--- app/routes/account-details.js
+++ app/routes/account-details.js
@@ -2,6 +2,13 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
     model(params){
+
+        // var account = this.store.createRecord('account', {
+        // _id: params.account_id
+        // });
+
+        // account.save();
+
         return this.store.findRecord('account', params.account_id);
     }    
 });
